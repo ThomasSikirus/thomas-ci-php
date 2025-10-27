706598c763dc01ba42555e107ef62f669b4323b4
Thomas Fagot <thomas.fagot@outlook.com>
2025-10-27 22:14:11 +0100

    Correction ou remise en forme de Math.php

--- src/Math.php
+++ src/Math.php
@@ -6,7 +6,7 @@ class Math
 {
         public function add(float $a, float $b): float
     {
-        return $a+$b; // style volontairement mauvais pour casser la CI
+        return $a + $b;
     }
 
 
